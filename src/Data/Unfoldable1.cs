7b1cbe3e5a7b33d69ffcfedcc582127929268472
0x1 <kevin.francart3@gmail.com>
2026-07-26 14:57:02 +0200

    Initial commit with C# FFI

---
+++ src/Data/Unfoldable1.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace Data.Unfoldable1;
+
+public static class FFI {
+    public static object Unfoldr1ArrayImpl(object arg1) => throw new NotImplementedException("Not implemented: Unfoldr1ArrayImpl");
+}
